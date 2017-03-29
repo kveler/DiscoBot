0171a2ce5a656ae44be2e0751162efca70bff827
Max Veenstra <m.veenstra@compra.nl>
2017-03-29 20:29:40 +0200

    De eerste komeet :D :D Spannend
    Echt vet spannend!

---
+++ DiscoBot/MyBot.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+
+namespace DiscoBot
+{
+    class MyBot
+    {
+        DiscordClient discord;
+        CommandService commands;
+
+        Random rand;
+
+        string[] randomBob;
+
+        public MyBot()
+        {
+
+            rand = new Random();
+
+            randomBob = new string[]
+            {
+                "bob/bob1.jpg",
+                "bob/bob2.jpg",
+                "bob/bob3.jpg",
+                "bob/bob4.jpg",
+                "bob/bob5.jpg",
+                "bob/bob6.jpg",
+                "bob/bob7.jpg"
+            };
+
+
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = true;
+            });
+
+            
+            commands = discord.GetService<CommandService>();
+
+            RegisterBobCommand();
+
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("Mjk2NjE2MDQxNDUzMTkxMTcw.C71Nlw.Mpl7EKEaWFg2pIX5ke9qcHsj8oo", TokenType.Bot);
+            });
+        }
+
+        private void RegisterBobCommand()
+        {
+            commands.CreateCommand("bob")
+                .Do(async (e) =>
+                {
+                    int randomBobIndex = rand.Next(randomBob.Length);
+                    string bobToPost = randomBob[randomBobIndex];
+                    await e.Channel.SendFile(bobToPost);
+                });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
